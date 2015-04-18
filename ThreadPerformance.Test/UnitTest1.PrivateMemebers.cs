1a42ff38ae448912995884b2c471ee6fd6a005d9
kuritka <kuritka@gmail.com>
2015-04-18 20:10:35 +0200

    Performance in multi-threading environment
    Multithread can rapidly improove performance. Unfortunatelly we can’t
    scale number of threads  endlessly. Each OS and .NET has its own thread
    settings and there is no concrete value of max thread limit. But it can
    be easily calculated. Using a lot of threads is bad not only because of
    their consumption.

---
+++ ThreadPerformance.Test/UnitTest1.PrivateMemebers.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NCommon.Extensions;
+
+namespace ThreadPerformance.Test
+{
+public partial class UnitTest1
+{
+    private sealed class State
+    {
+        public ManualResetEvent ResetEvent { get; set; }
+        public long Load { get; set; }
+    }
+
+    private readonly Action<long> _threadAction = load =>
+    {
+        var k = 2;
+        for (var j = 0; j < load; j++)
+        {
+            k += k * k + j;
+        }
+    };
+
+    private readonly Action<Action<long>,State> _threadSuperAction 
+        = (action, load) =>
+    {
+        action.Invoke(load.Load);
+        load.ResetEvent.Set();
+    };
+
+    private double MultiThreadCompute(int threadCount, int threadLoad)
+    {
+        var list = new List<Thread>();
+
+        Enumerable.Range(0, threadCount)
+            .ForEach(d => list.Add(
+                new Thread(load => _threadAction(threadLoad)) 
+                { Name = string.Format("thread{0:D4}", d) }));
+
+        var s1 = DateTime.Now;
+        list.ForEach(d => d.Start(threadLoad));
+        list.ForEach(d => d.Join());
+        return (DateTime.Now - s1).TotalMilliseconds;
+    }
+
+
+    private double SingleThreadCompute(long threadLoad, Action<long> action)
+    {
+        var s1 = DateTime.Now;
+        action(threadLoad);
+        return (DateTime.Now - s1).TotalMilliseconds;
+    }
+        
+    private double MultiThreadComputeWithThreadPool(int threadCount, long threadLoad)
+    {
+        var events = new List<State>(threadCount);
+        ThreadPool.SetMaxThreads(threadCount, threadCount);
+        for (int i = 0; i < threadCount; i++)
+        {
+            var state = new State {Load = threadLoad, ResetEvent 
+                = new ManualResetEvent(false)};
+            events.Add(state);
+            ThreadPool.QueueUserWorkItem(o=>
+                _threadSuperAction(_threadAction,state), state);
+        }
+
+        var s1 = DateTime.Now;
+        WaitHandle.WaitAll( events.Select(d=>d.ResetEvent).ToArray());
+        return (DateTime.Now - s1).TotalMilliseconds;
+    }
+
+
+
+    public double MultiThreadComputeWithTpl(int threadCount, long threadLoad)
+    {
+        var items = new List<long>();
+        Enumerable.Range(0, threadCount).ForEach(d => items.Add(threadLoad));
+        var s1 = DateTime.Now;
+        Parallel.ForEach(items, item => _threadAction(item));
+        return (DateTime.Now - s1).TotalMilliseconds;
+    }
+
+}
+}
---
+++ ThreadPerformance.Test/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace ThreadPerformance.Test
+{
+
+
+[TestFixture]
+partial class UnitTest1
+{
+
+    private readonly  int[] _threadCounts =
+    {
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 
+        19, 20, 21, 22, 23, 24, 32, 48, 64, 128, 256, 512, 1024,2048
+        , 4096, 8192, 16384, 32768, 65536
+    };
+
+    [Test]
+    public void ThreadClassTest()
+    {
+        const int load = 10 * 1000 * 1000;
+        foreach (var threadCount in _threadCounts)
+        {
+            var multiThreadTotalMiliseconds = MultiThreadCompute(threadCount, load / threadCount);
+            var singleThreadTotalMiliseconds = SingleThreadCompute(load, _threadAction);
+            Console.WriteLine("MultiThread ({0:D5}): {1}ms; SingleThread: {2}ms", threadCount, 
+                multiThreadTotalMiliseconds, singleThreadTotalMiliseconds);
+        }
+    }
+        
+    [Test]
+    public void ParalelForEachTest()
+    {
+        const int load = 10 * 1000 * 1000;
+            
+        foreach (var threadCount in _threadCounts)
+        {
+            var multiThreadTotalMiliseconds = MultiThreadComputeWithTpl(threadCount, load / threadCount);
+            var singleThreadTotalMiliseconds = SingleThreadCompute(load, _threadAction);
+            Console.WriteLine("Pool MultiThread ({0:D5}): {1}ms; SingleThread: {2}ms", threadCount, 
+                multiThreadTotalMiliseconds, singleThreadTotalMiliseconds);
+        }
+    }
+
+    [Test]
+    public void ThreadPoolTest()
+    {
+        const int load = 10 * 1000 * 1000;
+         
+        foreach (var threadCount in _threadCounts.Where(d=>d <=64))
+        {
+            var multiThreadTotalMiliseconds = MultiThreadComputeWithThreadPool(threadCount, load / threadCount);
+            var singleThreadTotalMiliseconds = SingleThreadCompute(load , _threadAction);
+            Console.WriteLine("Pool MultiThread ({0:D5}): {1}ms; SingleThread: {2}ms", threadCount, 
+                multiThreadTotalMiliseconds, singleThreadTotalMiliseconds);
+        }
+    }
+}
+}
